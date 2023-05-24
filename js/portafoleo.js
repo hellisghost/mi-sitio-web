f8fce26237b11bcc14aa808e8d12062c8f4145c2
hellisghost <andrademahechafernando@gmail.com>
2023-05-24 16:38:23 -0500

    ya

--- js/portafoleo.js
+++ js/portafoleo.js
@@ -1,4 +1,9 @@
 //funcion que muestra el menu responsive
 function responsivemenu(){
-    
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
 }
