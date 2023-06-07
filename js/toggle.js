9901f7b795279551bf717f066668821aaefcfea3
hellisghost <andrademahechafernando@gmail.com>
2023-06-07 16:16:39 -0500

    no se acabado

---
+++ js/toggle.js
@@ -0,0 +1,7 @@
+var caja = document.getElementById('para');
+var body = document.querySelector('body');
+
+caja.onclick = function(){
+    para.classList.caja('active');
+    body.classList.caja('active');
+}
