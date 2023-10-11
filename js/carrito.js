77664138b402c268fca354b2e230064852fa5958
hellisghost <andrademahechafernando@gmail.com>
2023-10-11 16:32:29 -0500

    carrito terminado

--- js/carrito.js
+++ js/carrito.js
@@ -1,12 +1,18 @@
-let boxminus =  document.getElementById("boxminus")
-let boxcantidad  = document.getElementById("boxcantidad")
-let boxplus = document.getElementById("boxplus")
+let boxminus =  document.getElementById("boxminus");
+let boxcantidad  = document.getElementById("boxcantidad");
+let boxplus = document.getElementById("boxplus");
+let valorproducto  = 37000000;
+let auxvalorproducto = new Intl.NumberFormat("de-DE").format(valorproducto);
+document.getElementById("preciounidad").innerHTML = auxvalorproducto;
+
 
 boxplus.addEventListener("click",function(){
     let auxcantidad = boxcantidad.innerHTML;
     if(auxcantidad < 10){
         auxcantidad++;
         boxcantidad.innerHTML = auxcantidad;
+        Calcularsubtotal()
+        cantidad()
     };
 });
 
@@ -15,5 +21,18 @@ boxminus.addEventListener("click",function(){
     if(auxcantidad > 0){
         auxcantidad--;
         boxcantidad.innerHTML = auxcantidad;
+        Calcularsubtotal()
+        cantidad()
     };
 });
+
+function Calcularsubtotal(){
+    let auxcantidad = valorproducto * Number(boxcantidad.innerHTML);
+    let auxcantid = Intl.NumberFormat("de-DE").format(auxcantidad);
+        document.getElementById("subtotal").innerHTML= "$" + auxcantid;
+}
+
+function cantidad(){
+    let auxcantidad = boxcantidad.innerHTML;
+    document.getElementById("numeroproductos").innerHTML  = auxcantidad
+}
