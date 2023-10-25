785fae3b06712908b920a3dd8219f43707645c73
hellisghost <andrademahechafernando@gmail.com>
2023-10-25 16:45:39 -0500

    sigir amazon

---
+++ js/amazon.js
@@ -0,0 +1,6 @@
+const cerrarmodal = document.getElementById("close");
+const modal = document.getElementById("modal");
+
+cerrarmodal.addEventListener("click", function() {
+modal.style.display = "none";
+})
