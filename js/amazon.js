58d273c23785f01dcfe6dd7e29209afadce5b868
hellisghost <andrademahechafernando@gmail.com>
2023-11-01 12:57:13 -0500

    segir

--- js/amazon.js
+++ js/amazon.js
@@ -1,6 +1,17 @@
 const cerrarmodal = document.getElementById("close");
 const modal = document.getElementById("modal");
+const abrirmodal = document.getElementById("imagenabrir");
+
+abrirmodal.addEventListener("click", function(){
+    modal.style.display  = "block";
+});
 
 cerrarmodal.addEventListener("click", function() {
-modal.style.display = "none";
-})
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event){
+    if (event.target = modal){
+        modal.style.display = "none";
+    };
+});
