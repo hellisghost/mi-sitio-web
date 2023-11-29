4a716718fe837d200db86d95a3a92d8c677eefb9
hellisghost <andrademahechafernando@gmail.com>
2023-11-29 16:05:26 -0500

    cambios

--- js/estrellas.js
+++ js/estrellas.js
@@ -2,34 +2,34 @@
 var totaltodo = document.querySelector("#totaltodo")
 var resultado = document.querySelector("#resultado")
 var uno = document.querySelector("#uno")
-var dos  =document.querySelector("#dos")
+var dos = document.querySelector("#dos")
 var tres = document.querySelector("#tres")
 var cuatro = document.querySelector("#cuatro")
 var cinco = document.querySelector("#cinco")
 
 var barrauno = document.querySelector("#estrellauno")
-var barrados  =document.querySelector("#estrellados")
+var barrados = document.querySelector("#estrellados")
 var barratres = document.querySelector("#estrellatres")
 var barracuatro = document.querySelector("#estrellacuatro")
 var barracinco = document.querySelector("#estrellacinco")
 
-totaltodo.addEventListener("click", function(){
+totaltodo.addEventListener("click", function () {
     solucion = parseInt(uno.value) + parseInt(dos.value) + parseInt(tres.value) + parseInt(cuatro.value) + parseInt(cinco.value)
-    resultado.innerHTML= `total de numero encuestas: ${solucion}`
+    resultado.innerHTML = `total de numero encuestas: ${solucion}`
 
-    var estrellacinco = parseInt(cinco.value)*100 / solucion
+    var estrellacinco = parseInt(cinco.value) * 100 / solucion
     barracinco.style.width = `${estrellacinco}%`
 
-    var estrellacuatro = parseInt(cuatro.value)*100 / solucion
+    var estrellacuatro = parseInt(cuatro.value) * 100 / solucion
     barracuatro.style.width = `${estrellacuatro}%`
 
-    var estrellatres = parseInt(tres.value)*100 / solucion
+    var estrellatres = parseInt(tres.value) * 100 / solucion
     barratres.style.width = `${estrellatres}%`
-    
-    var estrellados = parseInt(dos.value)*100 / solucion
+
+    var estrellados = parseInt(dos.value) * 100 / solucion
     barrados.style.width = `${estrellados}%`
 
-    var estrellauno = parseInt(uno.value)*100 / solucion
+    var estrellauno = parseInt(uno.value) * 100 / solucion
     barrauno.style.width = `${estrellauno}%`
 
 })
